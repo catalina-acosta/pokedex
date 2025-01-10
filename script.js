b2b8d9fbaca4a95c8a1a44f05ebc1aa5b97f1d99
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-01-10 13:57:44 +0100

    added fetchPokemonSpieces() in order to access the color of each pokemon an add it to each card as a class

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let pokemonList;
 let pokemonAbilities;
 let pokemonTypes;
-
-
+let pokemonColor;
 
 async function renderPokemon() {
     let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=40&offset=0');
@@ -15,21 +14,40 @@ async function renderPokemon() {
         let responsePokeCharJson = await responsePokeChar.json();
         pokemonAbilities = responsePokeCharJson;
         pokemonTypes = pokemonAbilities.types;
-        contentRef.innerHTML += pokemonCardTemplate(pokemonAbilities);
+
+        await fetchPokemonSpecies(pokemonAbilities.id);
+
+        contentRef.innerHTML += pokemonCardTemplate(pokemonAbilities, pokemonColor);
     }
 }
 
-function pokemonCardTemplate(pokemonAbilities) {
+async function fetchPokemonSpecies(id) {
+    let responsePokeSpecies = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
+    let responsePokeSpeciesJson = await responsePokeSpecies.json();
+
+    pokemonColor = responsePokeSpeciesJson.color.name;
+    return pokemonColor;
+}
+
+
+function pokemonCardTemplate(pokemonAbilities, pokemonColor) {
     return `
-    <div id="card">
+    <div id="card${pokemonAbilities.id}" class="card ">
                 <div class="img-container">
                     <img src="${pokemonAbilities.sprites.other['official-artwork'].front_default}" alt="${pokemonAbilities.name}">
                 </div>
-                <div class="card-info">
+                <div class="card-info ${pokemonColor}">
                     <p>${pokemonAbilities.name}</p>
-                    <p>${pokemonTypes[0].type.name}</p>
+                    <p>${pokemonTypes[0].type.name}</p> 
                 </div>
             </div>
     `
-
 }
+
+// FRAGEN
+
+// Warum pokemonTypes[1].type.name funkzioniert nicht? 
+
+// um das loading circle zu machen, braucht man ein timeout() function? 
+
+// pokemon haben 2 Typen. Welche sollte man für die hintergrund farbe nehmen? 
