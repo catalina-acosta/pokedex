4bd75bc8e7e9bb806572d5131955610d893427a1
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-01-17 17:35:09 +0100

    refractoring search bar done

--- searchBar.js
+++ searchBar.js
@@ -7,22 +7,25 @@ function searchPokemon(){
 
     let inputRef = document.getElementById("input").value;
     let contentRef = document.getElementById("content");
-    contentRef.innerHTML = "";
-
     let loadMoreBtnRef = document.getElementById("loadMoreBtnContainer");
+    contentRef.innerHTML = "";
     loadMoreBtnRef.innerHTML = "";
 
     if (inputRef.length > 2) {
-        allPokemonWithAbilities.forEach(pokemon => {
+        displayFilteredPokemon(inputRef, contentRef, loadMoreBtnRef);
+    } else {
+        contentRef.innerHTML += `<p>please enter more that 2 letters</p>`
+    }
+}
+
+function displayFilteredPokemon(inputRef, contentRef, loadMoreBtnRef) {
+    allPokemonWithAbilities.forEach(pokemon => {
             if (pokemon.name.includes(inputRef)) {
                 contentRef.innerHTML += pokemonCardTemplate(pokemon);
                 let pokemonTypesRef = document.getElementById(`pokemonTypes${pokemon.id}`);
                 pokemonTypes = pokemon.types;
                 pokemonTypesRef.innerHTML += createPokemonTypesTemplate(pokemonTypes);
             }
-        })
-    } else {
-        contentRef.innerHTML += `<p>please enter more that 2 letters</p>`
-    }
-    loadMoreBtnRef.innerHTML += `<button class="see-all-btn" onclick="resetpokemonAmountToBeRendered(), render40Pokemon() ">see all pokemon</button>`
+        });
+        loadMoreBtnRef.innerHTML += `<button class="see-all-btn" onclick="resetpokemonAmountToBeRendered(), render40Pokemon() ">see all pokemon</button>`
 }
